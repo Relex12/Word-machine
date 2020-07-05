d7d77b711577c3d4f0119d72ef58065d5ba8a57d
Adrian Bonnet <relex12@free.fr>
2020-07-05 16:48:10 +0200

    Update code in word_machine.py, add options for command line (no description for now)

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ A word generator from dictionary in Python
 
 ## Description
 
-This repository provides a word generator based on statistical analysis of an input dictionary. 
+This repository provides a word generator based on statistical analysis of an input dictionary.
 
 The algorithm is based on two main methods : the first one reads the dictionary and completes a matrix by counting how many times each a letter is followed by each other letter. The second algorithm uses these statistics to create a new word that looks like the words from the dictionary.
 
 ## Original idea
 
-The generator and how it works have been imagined in the first place by David Louapre, a French science popularizer. 
+The generator and how it works have been imagined in the first place by David Louapre, a French science popularizer.
 
 Please make sure to watch his video : [La machine à inventer des mots (avec Code MU) — Science étonnante #17](https://www.youtube.com/watch?v=YsR7r2378j0). It only has auto-generated subtitles and it talks about the French language, but I'm sure it will be OK.
 
@@ -27,27 +27,27 @@ This section will be done in the next commit.
 
 The code requires exactly two input files.
 
-The first file is an alphabet that contains every character that can contained is the dictionary words. The alphabet file must be written as a single space separated list of characters. All the characters must be on the first line, without carriage return. Upper and lower case letters must be both specified if needed. Each accent, digit or special character that is used have to be specified. Characters in the alphabet that are not used in the dictionary words are a loss of memory space, but the program still works. 'a' to 'z' characters and '-' are the often required. 
+The first file is an alphabet that contains every character that can contained is the dictionary words. The alphabet file must be written as a single space separated list of characters. All the characters must be on the first line, without carriage return. Upper and lower case letters must be both specified if needed. Each accent, digit or special character that is used have to be specified. Characters in the alphabet that are not used in the dictionary words are a loss of memory space, but the program still works. 'a' to 'z' characters and '-' are the often required.
 
-The second file contains the dictionary that will be used. It must be represented as a carriage return separated list. There should be no space between the word and the carriage return character. LF and CRLF distinction should not be a problem. There is no need to order the word. **Duplicated words are counted only once.** 
+The second file contains the dictionary that will be used. It must be represented as a carriage return separated list. There should be no space between the word and the carriage return character. LF and CRLF distinction should not be a problem. There is no need to order the word. **Duplicated words are counted only once.**
 
 ## Dictionary processing
 
 As dictionary processing can be quite long, a method is given to help you manage it. This method returns a new word dictionary, ordered and without duplicates, a list of words that could be duplicated between singular and plural forms, and a list of characters that have been found at least once in the dictionary and that are not in the alphabet file.
 
-The list of words that could be duplicated between singular and plural forms is based on if the word already exists without the final 'S' or not. This might not work if you're using a language that does not necessarily includes an 'S' in the plural, such as German or French (form "-al" and "-ail" words for example).
+The list of words that could be duplicated between singular and plural forms is based on if the word already exists without the final 'S' or not. This might not work if you're using a language that does not necessarily includes an 'S' in the plural, such as German or French ("-al" and "-ail" words for example). These words are not removed since you might want to keep them.
 
 ## A bit deeper in the code
 
-As the code have been developed on Atom, the input files can contain a final `'\n'` character. It is systematically removed when the file are loaded. All eventual empty words (`''`) are removed when the dictionary is loaded.
+As the code have been developed on Atom, the input files can contain a final `'\n'` character. It is systematically removed when the files are loaded. Every eventual empty word (`''`) is removed when the dictionary is loaded.
 
-An empty character is automatically added to the alphabet. This `''` character is used for letter that are at the beginning or at the end a word.
+An empty character is automatically added to the alphabet. This `''` character is used for letters that are at the beginning or at the end a word.
 
-The built matrix is actually three dimensional, so it can read to character before instead of one. For example, when "age" is ridden, the values in the `['']['']['a']`, `['']['a']['g']`, `['a']['g']['e']`, `['g']['e']['']` and `['e']['']['']` boxes in the matrix are increased. 
+The built matrix is actually three dimensional, so it can read two characters before instead of one. For example, when the word "age" is ridden, the values in the `['']['']['a']`, `['']['a']['g']`, `['a']['g']['e']`, `['g']['e']['']` and `['e']['']['']` boxes in the matrix are increased.
 
-A good example of why this is needed is the double letter probability. If a double "R" or "L" is quite common, it is very rare to see three R following each other. 
+A good example of why this is needed is the double letter probability. If a double "R" or "L" is quite common, it is very rare to see three R following each other.
 
-The code provides methods for two dimensional matrix, but the results are pretty dismal. 
+The code provides methods for two dimensional matrix, but the results are pretty dismal.
 
 Technically, increasing the dimension of the matrix is always a good way to improve the results.
 
@@ -61,13 +61,10 @@ Another way to improve the quality would be to create a method that enable the v
 
 As this repository probably won't be very active, please message me be email to coordinate an eventual Pull Request.
 
-## License
-
-The project was a short one, the author worked alone on this for a short time. The code is given to the GitHub Community for free, only under th MIT License, that is not too restrictive.
-
 ## Python version
 
-As long as the `ramdon.choices()` method is used to create the words, Python 3.6 or more is required. 
+As long as the `ramdon.choices()` method is used to create the words, Python 3.6 or more is required.
 
+## License
 
-
+The project is a small one. The code is given to the GitHub Community for free, only under th MIT License, that is not too restrictive.
--- word_machine.py
+++ word_machine.py
@@ -1,6 +1,25 @@
 from random import choices
+import sys
 
-def open_alphabet (filename):
+##################################
+# Command line option processing #
+##################################
+
+def get_option(option):
+    position = sys.argv.index(option)
+    if len(sys.argv) == position or sys.argv[position+1][0] == '-':
+        # raise the error instead of printing it
+        print ("Error : Missing argument for '"+option+"' option")
+    # no else need if raised
+    else:
+        return sys.argv[position+1]
+
+####################################
+# Input and output file management #
+####################################
+
+def open_alphabet (filename="alphabet.txt"):
+    # attention au \n si il n'y a pas d'espace final
     f = open(filename, "r")
     alphabet = f.read().split(' ')
     f.close()
@@ -9,7 +28,7 @@ def open_alphabet (filename):
     alphabet.insert(0, '')
     return alphabet
 
-def open_dictionary(filename):
+def open_dictionary(filename="dictionary.txt"):
     f = open(filename, "r")
     dictionary = f.read().split('\n')
     f.close()
@@ -17,21 +36,73 @@ def open_dictionary(filename):
         dictionary.remove('')
     return dictionary
 
-def test_dictionary (dictionary, alphabet):
-    dictionary = sorted(set(dictionary))
-    plural_words = []
+def write_clean_dictionary(dictionary, filename="output_dictionary.txt"):
+    f = open(filename, 'w')
+    for word in dictionary:
+        f.write(word+'\n')
+    f.close()
+
+def write_generated_words(word_list, filename="generated_words.txt"):
+    f = open(filename, 'w')
+    f.write(word_list)
+    f.close()
+
+#########################
+# Dictionary processing #
+#########################
+
+def process_dictionary (dictionary):
+    return sorted(set(dictionary))
+
+def remove_unknown_letters(dictionary, missing_letters):
+    for i in range(1, 10):
+        for letter in missing_letters:
+            for word in dictionary:
+                if letter in word:
+                    # print (letter, word)
+                    dictionary.remove(word)
+                    # print (word in dictionary)
+    return dictionary
+
+def get_missing_letters (dictionary, alphabet):
     missing_letter = []
+    for word in dictionary:
+        for letter in word:
+            if not letter in alphabet and not letter in missing_letter:
+                missing_letter.append(letter)
+    return (missing_letter)
+
+def remove_plural_words (dictionary):
+    # incomplete
+    for word in dictionary:
+        if word[len(word)-1] == 's':
+            if word[:len(word)-1] in dictionary:
+                dictionary.remove(word)
+    return (dictionary)
+
+def get_plural_words (dictionary):
+    # incomplete
+    plural_words = []
     for word in dictionary:
         if word[len(word)-1] == 's':
             if word[:len(word)-1] in dictionary:
                 plural_words.append(word)
-        for letter in word:
-            if not letter in alphabet and not letter in missing_letter:
-                missing_letter.append(letter)
-    print (plural_words)
-    print (missing_letter)
-    # problème : renvoi d'objets vides
-    return (dictionary, plural_words, missing_letter)
+    return (plural_words)
+
+def lower_case_words(dictionary):
+    return ([word.lower() for word in dictionary])
+
+def remove_acronyms (dictionary):
+    for i in range (1, 4):
+        for word in dictionary:
+            if word == word.upper():
+                print (word)
+                dictionary.remove(word)
+    return (dictionary)
+
+###############################################
+# 2 dimensions matrix, only one letter before #
+###############################################
 
 def initiate_empty_2D_matrix(alphabet):
     matrix = dict()
@@ -65,6 +136,10 @@ def generate_word_2D(matrix, alphabet):
         previous_letter = new_letter
     return (word)
 
+###########################################
+# 3 dimensions matrix, two letters before #
+###########################################
+
 def initiate_empty_3D_matrix(alphabet):
     matrix = dict()
     for letter1 in alphabet:
@@ -86,7 +161,7 @@ def build_3D_matrix(matrix, dictionary):
         matrix[word[len(word)-2]][word[len(word)-1]][''] +=1
         matrix[word[len(word)-1]][''][''] +=1
 
-def generate_word_2D(matrix, alphabet):
+def generate_word_3D(matrix, alphabet):
     word = ''
     previous_letter1 = ''
     previous_letter2 = ''
@@ -98,12 +173,44 @@ def generate_word_2D(matrix, alphabet):
         previous_letter2 = new_letter
     return (word)
 
+
+#############################
+# Main zone : executed code #
+#############################
+
 if __name__ == '__main__':
 
-    alphabet = open_alphabet ("alphabet2.txt")
-    dictionary = open_dictionary("légendaires-copie")
+    # getting alphabet
+    if '-a' in sys.argv:
+        alphabet = open_alphabet(get_option ('-a'))
+    else:
+        alphabet = open_alphabet()
 
-#    print (test_dictionary(dictionary, alphabet)[1:2])
+    # getting dictionary
+    if '-d' in sys.argv:
+        dictionary = open_dictionary(get_option('-d'))
+    else:
+        dictionary = open_dictionary()
+
+    dictionary = process_dictionary(dictionary)
+
+    if '-ac' in sys.argv:
+        dictionary = remove_acronyms(dictionary)
+
+    if '-lc' in sys.argv:
+        dictionary = lower_case_words(dictionary)
+
+    missing_letters = get_missing_letters(dictionary, alphabet)
+    if '-r' in sys.argv:
+        dictionary = remove_unknown_letters (dictionary, missing_letters)
+    else:
+        if missing_letters != []:
+            print ('WARNING: Some characters are used in the dictionary without being in the alphabet')
+            print (missing_letters)
+
+    if '-w' in sys.argv:
+        write_clean_dictionary (dictionary)
+
 
 ###############################################
 # 2 dimensions matrix, only one letter before #
@@ -119,9 +226,32 @@ if __name__ == '__main__':
 # 3 dimensions matrix, two letters before #
 ###########################################
 
-    matrix_3D = initiate_empty_3D_matrix(alphabet)
+    if '-g' in sys.argv:
+        matrix_3D = initiate_empty_3D_matrix(alphabet)
+        build_3D_matrix(matrix_3D, dictionary)
 
-    build_3D_matrix(matrix_3D, dictionary)
+        output_file = '-o' in sys.argv
+        word_list = ""
 
-    for i in range(1, 100):
-        print (generate_word_2D(matrix_3D, alphabet))
+        if '-s' in sys.argv:
+            required_size = int(get_option('-s'))
+            i = 0
+            while i != int(get_option('-g')):
+                word = generate_word_3D(matrix_3D, alphabet)
+                if len(word) == required_size:
+                    if output_file:
+                        word_list += word + '\n'
+                    else:
+                        print (word)
+                    i-=-1
+
+        else:
+            for i in range(1, int(get_option('-g'))):
+                word = generate_word_3D(matrix_3D, alphabet)
+                if output_file:
+                    word_list += word + '\n'
+                else:
+                    print (word)
+
+        if output_file:
+            write_generated_words(word_list, get_option('-o'))

a703c90704f21fa3f00ec8996c2f92f3b50809c0
Adrian Bonnet <relex12@free.fr>
2020-07-06 21:36:37 +0200

    Update Readme, add many options to command line (see Readme)

--- word_machine.py
+++ word_machine.py
@@ -5,21 +5,32 @@ import sys
 # Command line option processing #
 ##################################
 
-def get_option(option):
-    position = sys.argv.index(option)
-    if len(sys.argv) == position or sys.argv[position+1][0] == '-':
-        # raise the error instead of printing it
-        print ("Error : Missing argument for '"+option+"' option")
-    # no else need if raised
+class MissingArgumentError(BaseException):
+    """docstring for MissingArgumentError."""
+
+    def __init__(self, opt):
+        self.opt = opt
+        self.text = "Missing argument for "
+
+def get_option(option, mandatory):
+    position = sys.argv.index(option) + 1
+    if len(sys.argv) == position or sys.argv[position][0] == '-':
+        if mandatory:
+            try:
+                raise MissingArgumentError (option)
+                # the function exits properly, then a TypeError stops the run
+                # it should stop in this function instead of exiting it
+            except MissingArgumentError as e:
+                print ("Error : Missing argument for {0} option".format(option))
+        return None
     else:
-        return sys.argv[position+1]
+        return sys.argv[position]
 
 ####################################
 # Input and output file management #
 ####################################
 
 def open_alphabet (filename="alphabet.txt"):
-    # attention au \n si il n'y a pas d'espace final
     f = open(filename, "r")
     alphabet = f.read().split(' ')
     f.close()
@@ -55,13 +66,14 @@ def process_dictionary (dictionary):
     return sorted(set(dictionary))
 
 def remove_unknown_letters(dictionary, missing_letters):
-    for i in range(1, 10):
-        for letter in missing_letters:
-            for word in dictionary:
-                if letter in word:
-                    # print (letter, word)
-                    dictionary.remove(word)
-                    # print (word in dictionary)
+    words_to_del = []
+    for letter in missing_letters:
+        for word in dictionary:
+            if letter in word:
+                words_to_del.append(word)
+    words_to_del = set(words_to_del)
+    for word in words_to_del:
+        dictionary.remove(word)
     return dictionary
 
 def get_missing_letters (dictionary, alphabet):
@@ -72,32 +84,49 @@ def get_missing_letters (dictionary, alphabet):
                 missing_letter.append(letter)
     return (missing_letter)
 
-def remove_plural_words (dictionary):
-    # incomplete
-    for word in dictionary:
-        if word[len(word)-1] == 's':
-            if word[:len(word)-1] in dictionary:
-                dictionary.remove(word)
-    return (dictionary)
+def print_plural_words (dictionary, lang):
+    if lang == 'fr':
+        for word in dictionary:
+            l = len(word)
+            if word[l-1] == 's'and word[:l-1] in dictionary \
+            or word[l-1] == 'x'and word[:l-1] in dictionary \
+            or l > 3 and word[l-3:l] == 'aux'and word[:l-3]+'al' in dictionary \
+            or l > 3 and word[l-3:l] == 'aux'and word[:l-3]+'ail' in dictionary:
+                print(word)
+    # insert here plural rules from other languages
 
-def get_plural_words (dictionary):
-    # incomplete
-    plural_words = []
-    for word in dictionary:
-        if word[len(word)-1] == 's':
-            if word[:len(word)-1] in dictionary:
-                plural_words.append(word)
-    return (plural_words)
+def remove_plural_words (dictionary, lang):
+    words_to_del = []
+    if lang == 'fr':
+        for word in dictionary:
+            l = len(word)
+            if word[l-1] == 's'and word[:l-1] in dictionary \
+            or word[l-1] == 'x'and word[:l-1] in dictionary \
+            or l > 3 and word[l-3:l] == 'aux'and word[:l-3]+'al' in dictionary \
+            or l > 3 and word[l-3:l] == 'aux'and word[:l-3]+'ail' in dictionary:
+                words_to_del.append(word)
+    words_to_del = set(words_to_del)
+    for word in words_to_del:
+        dictionary.remove(word)
+    # insert here plural rules from other languages
+    return (dictionary)
 
 def lower_case_words(dictionary):
     return ([word.lower() for word in dictionary])
 
+def print_acronyms (dictionary):
+    for word in dictionary:
+        if word == word.upper():
+            print(word)
+
 def remove_acronyms (dictionary):
-    for i in range (1, 4):
-        for word in dictionary:
-            if word == word.upper():
-                print (word)
-                dictionary.remove(word)
+    words_to_del = []
+    for word in dictionary:
+        if word == word.upper():
+            words_to_del.append(word)
+    words_to_del = set(words_to_del)
+    for word in words_to_del:
+        dictionary.remove(word)
     return (dictionary)
 
 ###############################################
@@ -161,10 +190,19 @@ def build_3D_matrix(matrix, dictionary):
         matrix[word[len(word)-2]][word[len(word)-1]][''] +=1
         matrix[word[len(word)-1]][''][''] +=1
 
-def generate_word_3D(matrix, alphabet):
-    word = ''
-    previous_letter1 = ''
-    previous_letter2 = ''
+def generate_word_3D(matrix, alphabet, prefix):
+    if prefix == False:
+        word = ''
+        previous_letter1 = ''
+        previous_letter2 = ''
+    elif len(prefix) == 1:
+        word = prefix
+        previous_letter1 = ''
+        previous_letter2 = prefix[-1]
+    else:
+        word = prefix
+        previous_letter1 = prefix[-2]
+        previous_letter2 = prefix[-1]
     new_letter = None
     while new_letter != '':
         new_letter = choices(population=alphabet, weights=matrix[previous_letter1][previous_letter2].values(), k=1)[0]
@@ -173,6 +211,22 @@ def generate_word_3D(matrix, alphabet):
         previous_letter2 = new_letter
     return (word)
 
+def generate_3D_from_end(matrix, alphabet, suffix):
+    if len(suffix) == 1:
+        next_letter1 = suffix[0]
+        next_letter2 = ''
+    else:
+        next_letter1 = suffix[0]
+        next_letter2 = suffix[1]
+    word = suffix
+    new_letter = None
+    while new_letter != '':
+        new_letter = choices(population=alphabet, weights=matrix[next_letter1][next_letter2].values(), k=1)[0]
+        print (new_letter, word)
+        word = new_letter+word
+        next_letter1 = new_letter
+        next_letter2 = next_letter1
+    return (word)
 
 #############################
 # Main zone : executed code #
@@ -180,78 +234,94 @@ def generate_word_3D(matrix, alphabet):
 
 if __name__ == '__main__':
 
+    arg_list = ['-alpha', '-dict', '-write', '-gen', '-force', '-low-case', \
+                '-size', '-no-acronyms', '-output', '-prefix', '-no-plural', \
+                '-new','-print-acronyms', '-print-plural']
+
+    for arg in sys.argv:
+        if arg[0] == '-' and not arg in arg_list:
+            print ("Error: unrecognized argument", arg)
+
     # getting alphabet
-    if '-a' in sys.argv:
-        alphabet = open_alphabet(get_option ('-a'))
+    if '-alpha' in sys.argv:
+        alphabet = open_alphabet(get_option ('-alpha', True))
     else:
         alphabet = open_alphabet()
 
     # getting dictionary
-    if '-d' in sys.argv:
-        dictionary = open_dictionary(get_option('-d'))
+    if '-dict' in sys.argv:
+        dictionary = open_dictionary(get_option('-dict', True))
     else:
         dictionary = open_dictionary()
 
     dictionary = process_dictionary(dictionary)
 
-    if '-ac' in sys.argv:
+    if '-print-acronyms' in sys.argv:
+        print_acronyms (dictionary)
+
+    if '-print-plural' in sys.argv:
+        print_plural_words (dictionary, get_option('-print-plural', True))
+
+    if '-no-acronyms' in sys.argv:
         dictionary = remove_acronyms(dictionary)
 
-    if '-lc' in sys.argv:
+    if '-low-case' in sys.argv:
         dictionary = lower_case_words(dictionary)
 
+    if '-no-plural' in sys.argv:
+        dictionary = remove_plural_words(dictionary, get_option('-no-plural', True))
+
     missing_letters = get_missing_letters(dictionary, alphabet)
-    if '-r' in sys.argv:
+    if '-force' in sys.argv:
         dictionary = remove_unknown_letters (dictionary, missing_letters)
     else:
         if missing_letters != []:
             print ('WARNING: Some characters are used in the dictionary without being in the alphabet')
             print (missing_letters)
 
-    if '-w' in sys.argv:
-        write_clean_dictionary (dictionary)
+    if '-write' in sys.argv:
+        filename = get_option('-write', False)
+        if filename == None:
+            write_clean_dictionary (dictionary)
+        else:
+            write_clean_dictionary (dictionary, filename)
 
+    # if '-plot' in sys.argv:
+        # matrix_2D = initiate_empty_2D_matrix(alphabet)
+        # build_2D_matrix (matrix_2D, dictionary)
+        # plot_2D_matrix(matrix_2D, alphabet)
 
-###############################################
-# 2 dimensions matrix, only one letter before #
-###############################################
-
-    # matrix_2D = initiate_empty_2D_matrix(alphabet)
-    # build_2D_matrix (matrix_2D, dictionary)
-    # plot_2D_matrix(matrix_2D, alphabet)
-    # for i in range(1, 10):
-    #     print (generate_word_2D(matrix_2D, alphabet))
-
-###########################################
-# 3 dimensions matrix, two letters before #
-###########################################
-
-    if '-g' in sys.argv:
+    if '-gen' in sys.argv:
         matrix_3D = initiate_empty_3D_matrix(alphabet)
         build_3D_matrix(matrix_3D, dictionary)
 
-        output_file = '-o' in sys.argv
+        output_file = '-output' in sys.argv
         word_list = ""
 
-        if '-s' in sys.argv:
-            required_size = int(get_option('-s'))
-            i = 0
-            while i != int(get_option('-g')):
-                word = generate_word_3D(matrix_3D, alphabet)
-                if len(word) == required_size:
-                    if output_file:
-                        word_list += word + '\n'
-                    else:
-                        print (word)
-                    i-=-1
+        prefix = '-prefix' in sys.argv
+        if prefix:
+            prefix = get_option('-prefix', True)
 
-        else:
-            for i in range(1, int(get_option('-g'))):
-                word = generate_word_3D(matrix_3D, alphabet)
+        required_size = '-size' in sys.argv
+        if required_size:
+            required_size = int(get_option('-size', True))
+
+        new_only = '-new' in sys.argv
+
+        i = 0
+        while i != int(get_option('-gen', True)):
+            word = generate_word_3D(matrix_3D, alphabet, prefix)
+            if (required_size == False or len(word) == required_size) \
+            and not (new_only and word in dictionary):
                 if output_file:
                     word_list += word + '\n'
                 else:
                     print (word)
+                i-=-1
 
         if output_file:
-            write_generated_words(word_list, get_option('-o'))
+            filename = get_option('-output', False)
+            if filename == None:
+                write_generated_words(word_list)
+            else:
+                write_generated_words(word_list, filename)
